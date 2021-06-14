c9873be0bee5e323b413e8f6daef3077fe396590
yufree <yufree@live.cn>
2021-06-14 14:54:58 -0400

    add data

---
+++ R/data.R
@@ -0,0 +1,26 @@
+#' A dataset 177 peaks/metabolites from 1198 samples
+#' @docType data
+#' @usage data(meta)
+#' @format A data frame with 177 rows and 1198 columns:
+#' \describe{
+#'   177 peaks/metabolites from 1198 samples. Raw data are from Exposome Data Challenge 2021.
+#'   }
+"meta"
+
+#' A dataset with 7 exposures from 1198 samples
+#' @docType data
+#' @usage data(expo)
+#' @format A data frame with 7 rows and 1198 columns:
+#' \describe{
+#'   7 exposures from 1198 samples. Raw data are from Exposome Data Challenge 2021.
+#'   }
+"expo"
+
+#' A vector with asthma condition for 1198 samples
+#' @docType data
+#' @usage data(asthma)
+#' @format asthma condition for 1198 samples
+#' \describe{
+#'   1 means asthma and 0 means non-asthma. Raw data are from Exposome Data Challenge 2021.
+#'   }
+"asthma"
